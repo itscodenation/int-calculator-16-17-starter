d602f279ff5c0863e18e14154abc3e3a3c8e1284
Tom Bijesse <tom@bijesse.com>
2016-10-20 09:23:26 -0400

    code update

--- starter_code/js/guts.js
+++ starter_code/js/guts.js
@@ -5,40 +5,41 @@
 
 function divide(x,y)
 {
-	return x/y;
+	//Write your code below this line
+	
 }
 
 function multiply(x,y)
 {
-	return x*y;
+	
 }
 
 function subtract(x,y)
 {
-	return x-y;
+	
 }
 
 function add(x,y)
 {
-	return x+y;
+	
 }
 
 function power(x,y)
 {
-	return Math.pow(x,y);
+	
 }
 
 function square_root(x)
 {
-	return Math.sqrt(x);
+	
 }
 
 function logBase10(x)
 {
-	return Math.log(x) / Math.LN10;
+	
 }
 
 function generateRandomNumber()
 {
-	return Math.floor((Math.random() * 1000) + 1);
+	
 }
